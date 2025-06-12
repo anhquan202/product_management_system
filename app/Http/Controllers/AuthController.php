7ad0d582472d8da6911304b3a7d02351e0ca46dc
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2025-06-13 01:20:43 +0700

    Handle login by username; add api route

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enum\ResponseCode;
+use App\Helpers\Response;
+use App\Http\Requests\LoginByUsernameRequest;
+use App\Models\UserAccount;
+use App\Services\Auth\IAuthService;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    protected $iAuthService;
+    public function __construct(IAuthService $iAuthService)
+    {
+        $this->iAuthService = $iAuthService;
+    }
+    public function loginByUsername(LoginByUsernameRequest $request)
+    {
+        try {
+            $credentials = $request->validated();
+            $result = $this->iAuthService->loginByUsername($credentials);
+            if ($result['statusCode'] === ResponseCode::SUCCESS) {
+                return Response::success(['access_token' => $result['token']], 'Login Success', ResponseCode::SUCCESS)
+                    ->cookie('access_token', $result['token'], 60 * 15, '/', null, false, true);
+                ;
+            } else {
+                return Response::error('Login Failed', ResponseCode::UNAUTHORIZED);
+            }
+        } catch (\Throwable $th) {
+            return Response::error($th->getMessage(), ResponseCode::INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ app/Http/Requests/LoginByUsernameRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginByUsernameRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => 'required|string',
+            'password' => 'required|string|min:5'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'username.required' => 'Tên đăng nhập là bắt buộc.',
+            'username.string' => 'Tên đăng nhập phải là một chuỗi.',
+            'password.required' => 'Mật khẩu là bắt buộc.',
+            'password.string' => 'Mật khẩu phải là một chuỗi.',
+            'password.min' => 'Mật khẩu phải có ít nhất 5 ký tự.'
+        ];
+    }
+}
